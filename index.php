bf53f045452802e2022533f739dcc4f93797b298
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-02-24 23:09:56 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DirVort - The Future of Web Browsing</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <h1>DirVort</h1>
+      <p>The Future of Web Browsing</p>
+    </header>
+    <main>
+      <h2>Browse the web with the latest technology</h2>
+      <p>DirVort is a revolutionary web browser that combines the latest advancements in artificial intelligence and virtual reality to create a seamless and immersive browsing experience. With DirVort, you can explore the web in a whole new way.</p>
+      <a href="#" class="cta-btn">Get started</a>
+    </main>
+    <footer>
+      <p>&copy; 2023 DirVort. All rights reserved.</p>
+    </footer>
+  </body>
+</html>
